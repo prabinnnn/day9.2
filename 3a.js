c8ad1cc867a13f1f49662754a653aacae400b1cb
prabin <bhandariprabin402@gmail.com>
2023-12-25 17:50:30 +0545

    add 3b

--- 3a.js
+++ 3a.js
@@ -1,11 +1,11 @@
 function returnlarger(arr, num) {
   let newarr = [];
-  for (let i = 0; i < Arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > num) {
       newarr.push(arr[i]);
     }
   }
   return newarr;
 }
-console.log(returnLarger([65, 16, 0, 6, 64, 1, 68], 16));
-console.log(returnLarger([6, 46, 54, 6, 56, 54, 65, 4, 65], 50));
+console.log(returnlarger([65, 16, 0, 6, 64, 1, 68], 16));
+console.log(returnlarger([6, 46, 54, 6, 56, 54, 65, 4, 65], 50));
---
+++ 3b.js
@@ -0,0 +1,11 @@
+function returnlarger(arr, num) {
+  return arr.reduce((acc, curr) => {
+    if (curr>acc)
+    {
+ return acc.concat(crr);
+    }
+    else{
+        return acc;
+    }
+  }
+}
